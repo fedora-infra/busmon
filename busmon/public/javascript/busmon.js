815490342cc49fa66605b9364c1e85eb32ca55f2
Ralph Bean <rbean@redhat.com>
2012-03-23 20:50:59 -0400

    User notifications about matched/unmatched topics.

--- busmon/public/javascript/busmon.js
+++ busmon/public/javascript/busmon.js
@@ -12,18 +12,40 @@ $.extend(busmon, {
     },
 
     apply_new_regex: function(value) {
-        // Take in the new value
+        // Take in the new value.  This will affect all callbacks passed
+        // through busmon.filter(callback, json);
         var re = new RegExp(value);
         busmon.regex = re;
 
         // Hard-coded id here... :/
         var selector = "topics-bar-chart";
 
+        // We're going to run through all the items in the bar chart and remove
+        // any that are not matched by the new regular expr.
+        var removed = [];
+        var kept = [];
         var keys = tw2.d3.util.keys(selector);
         for (var i = 0; i < keys.length; i++) {
             if (!busmon.regex.test(keys[i])) {
                 tw2.d3.util.remove_key(selector, keys[i]);
+                removed.push(keys[i]);
+            } else {
+                kept.push(keys[i]);
             }
+        }
+
+        // Notify the user about matches
+        if ( value == "" ) {
+            $.gritter.add({title: "busmon", text: "Filter removed.",});
+        } else {
+            $.gritter.add({
+                title: "busmon",
+                text: "Matched " + kept.join(", "),
+            });
+            $.gritter.add({
+                title: "busmon",
+                text: "Removed " + removed.join(", "),
+            });
         }
     },
 });

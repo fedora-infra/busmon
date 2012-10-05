c30ba2d2a4fae1fbfd6059111a503fb764ef96c6
Ralph Bean <rbean@redhat.com>
2012-10-05 10:11:33 -0400

    0.4.2

--- busmon_consumers/setup.py
+++ busmon_consumers/setup.py
@@ -21,7 +21,7 @@ install_requires=[
 
 setup(
     name='busmon_consumers',
-    version='0.4.1',
+    version='0.4.2',
     description='fedmsg-hub consumers for the busmon webapp',
     author='Ralph Bean',
     author_email='rbean@redhat.com',
